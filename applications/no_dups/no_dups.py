7c3e3e9a2a29ccc4f653b387021c5fa13190055f
Nate Dow <ndow33@gmail.com>
2020-09-30 16:30:46 -0600

    update

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,21 @@
 def no_dups(s):
-    # Your code here
+    # split s into individual words
+    s = s.split()
+    # create a dictionary to hold the words
+    d = {}
+    new_string = ''
+    # loop through the words
+    for word in s:
+        # if the word is not in the dictionary
+        if word not in d:
+            # add it to the dictionary
+            d[word] = word
+            new_string = new_string + word + ' '
+
+    new_string = new_string[:-1]
+
+    return new_string
+    
 
 
 
