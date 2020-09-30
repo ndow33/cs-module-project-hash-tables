5dec2a140285b1983c4a56519bf3b1b07c39953e
Nate Dow <ndow33@gmail.com>
2020-09-30 15:12:22 -0600

    update

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,25 @@
 # Your code here
+d = {}
 
 
 def expensive_seq(x, y, z):
-    # Your code here
+    # base case
+    if x <= 0:
+        t = (x, y, z)
+        # create a specific key for t
+        d[t] = y + z      
+        return d[t]
+
+    if x > 0:    
+        # create a tuple holding the values of x, y, and z
+        t = (x, y, z)
+        # if t is not in the dictionary
+        if t not in d:
+            # calculate it and add it to d
+            d[t] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+            return d[t]
+        else:
+            return d[t]
 
 
 
