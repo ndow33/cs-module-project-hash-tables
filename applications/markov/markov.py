380080a6b66c9abae63d1b3315d92695774ad694
Nate Dow <ndow33@gmail.com>
2020-09-30 16:54:04 -0600

    update

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,35 @@
 import random
+import os
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications\markov\input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+words = words.split()
+# create a dictionary to house the words
+d = {}
+# loop through all the words in words
+counter = 0
+for word in words:
+    # if the word is not in the dictionary
+    if word not in d:
+        # create a list to be held in the dictionary
+        l = []
+        # add the word to the dictionary
+        # with an empty list as the value
+        d[word] = l
+    # once the word is in the dictionary
+    # get the next word
+    next_word = words[counter + 1]
+    # put that word in the list
+    d[word].append(next_word)
+    # increment counter
+    if counter < len(words)-2:
+        counter += 1   
 
 
 # TODO: construct 5 random sentences
 # Your code here
 
+print(d)
