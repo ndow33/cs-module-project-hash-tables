840ef05e5ac876c569b20ea3ed33cd57b4a2ac52
Nate Dow <ndow33@gmail.com>
2020-09-30 16:15:10 -0600

    update

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,29 @@
 def word_count(s):
-    # Your code here
+    # filter out the unwanted characters from the original string
+    pro = ['"', ':', ';', ',', '.', '-', '+', '=', '/', "\\", '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    for letter in s:
+        for char in pro:
+            if letter == char:
+                s = s.replace(letter, ' ')
+    # make the input lowercase
+    s = s.lower()
+    # split the sentence into a list of words
+    s = s.split()
+    # create a dictionary to house the word counts
+    d = {}
+    for word in s:
+        # loop through the prohibited values
+        #for 
+        # if the word is in the dictionary
+        if word in d:
+            # add 1 to its value
+            d[word] += 1
+        # if the word is not in the dictionary
+        else:
+            d[word] = 1
+    
+
+    return d
 
 
 
@@ -8,3 +32,5 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
